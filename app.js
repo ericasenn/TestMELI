d4e7d75ff1d5b2b3b3359405da4639327b065b41
Erica Senn <erica.senn@gmail.com>
2018-06-15 19:16:43 -0300

    Search endpoint

--- app.js
+++ app.js
@@ -1,9 +1,77 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var axios = require('axios');
+const URL_SEARCH = 'https://api.mercadolibre.com/sites/MLA/search';
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/api/items', function (req, res) {
+    axios.get(URL_SEARCH, {
+        params: {
+            q: req.query.q,
+            limit: 4
+        }
+    })
+    .then(function (response) {
+        const responseJSON = buildSearchResults(response.data);
+        res.send(responseJSON);
+    })
+    .catch(function (error) {
+        return Promise.reject(error);
+    });
 });
 
-app.listen(3000, function () {
+function buildAuthor() {
+    return {
+        "name": 'Erica Gretel',
+        "lastname": 'Senn'
+    }
+}
+
+function getCategories(filters) {
+    var categories = [];
+    const categoryFilters = filters.filter(function(filter) {
+        return filter.id == "category"
+    });
+
+    if(categoryFilters.length > 0) {
+        categoryFilters.forEach(function(filter) {
+            filter.values.forEach( function (category) {
+                var category = category.path_from_root.map(function(category) { return category.name });
+                categories.push(category);
+            })
+        });
+    }
+
+    return categories;
+}
+
+function getItems(results) {
+    return results.map( function(result) {
+        return {
+            "id": result.id,
+            "title": result.title,
+            "price": {
+                "currency": result.currency_id,
+                "amount": result.price,
+                "decimals": 2
+            },
+            "picture": result.thumbnail,
+            "condition": result.condition,
+            "free_shipping": result.shipping.free_shipping
+        };
+    });
+}
+
+function buildSearchResults(responseData) {
+    return {
+        "author": buildAuthor(),
+        "categories": getCategories(responseData.filters),
+        "items": getItems(responseData.results)
+    };
+}
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    host = (host === '::' ? 'localhost' : host);
+    var port = server.address().port;
+
+    console.log('listening at http://%s:%s', host, port);
 });
